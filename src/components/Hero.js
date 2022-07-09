ca03af95e228164ef33bf72d1b16e4377a7c46a3
babar <systemrockstar96@gmail.com>
2022-07-09 12:31:54 +0530

    added media queries to Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/Hero.css'
 import hero_img from '../assets/Pickup_Illustration.png'
+import scroll_img from '../assets/scroll.png'
 import {
   FaFacebookF,
   FaTwitter,
@@ -11,38 +12,47 @@ import {
 
 const Hero = () => {
   return (
-    <main className='hero'>
-      <h3 className='hero-head'>
-        vehicle maintenance from the comfort of your home
-      </h3>
-      <p className='hero-para'>
-        Open Auto soothes the hassle of maintaining your vehicle and helps you
-        deal with unexpected repairs worry-free.
-      </p>
-      <form className='form'>
-        <input type='text' placeholder='Enter Your Name' required />
-        <input type='email' placeholder='Enter Your Mail' required />
-        <button type='submit' className='submit-btn'>
-          Submit
-        </button>
-      </form>
-      <img src={hero_img} alt='hero' className='hero-img' />
-      <div className='social-links'>
-        <a href='#'>
-          <FaFacebookF size='1.5em' />
-        </a>
-        <a href='#'>
-          <FaTwitter size='1.5em' />
-        </a>
-        <a href='#'>
-          <FaYoutube size='1.5em' />
-        </a>
-        <a href='#'>
-          <FaLinkedin size='1.5em' />
-        </a>
-        <a href='#'>
-          <FaInstagram size='1.5em' />
-        </a>
+    <main className='hero-container'>
+      <div className='hero'>
+        <div className='hero-form'>
+          <h3 className='hero-head'>
+            vehicle maintenance from the comfort of your home
+          </h3>
+          <p className='hero-para'>
+            Open Auto soothes the hassle of maintaining your vehicle and helps
+            you deal with unexpected repairs worry-free.
+          </p>
+          <form className='form'>
+            <input type='text' placeholder='Enter Your Name' required />
+            <input type='email' placeholder='Enter Your Mail' required />
+            <button type='submit' className='submit-btn'>
+              Submit
+            </button>
+          </form>
+        </div>
+        <img src={hero_img} alt='hero' className='hero-img' />
+      </div>
+      <div className='social-container'>
+        <div className='social-links'>
+          <a href='#'>
+            <FaFacebookF size='1.5em' />
+          </a>
+          <a href='#'>
+            <FaTwitter size='1.5em' />
+          </a>
+          <a href='#'>
+            <FaYoutube size='1.5em' />
+          </a>
+          <a href='#'>
+            <FaLinkedin size='1.5em' />
+          </a>
+          <a href='#'>
+            <FaInstagram size='1.5em' />
+          </a>
+        </div>
+        <div className='scroll-img'>
+          <img src={scroll_img} alt='scroll' />
+        </div>
       </div>
     </main>
   )
